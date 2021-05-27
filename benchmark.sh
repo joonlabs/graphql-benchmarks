ac912b15bb2e3ea7871003020dc617c51d91eaa7
Julius Türich <info@joonlabs.com>
2021-05-28 00:29:34 +0200

    add comments to benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,19 +2,19 @@
 
 clear
 
-# define random variable pattern
+# Define random variable pattern
 pattern="{{RANDOMVARIABLE}}"
 
-# Here you can pre define different raw query-data
+# Here you can pre define different raw query-data. Your pattern gets replaced with a random number (during each iteration) to prevent server side caching
 query_no_complexity='{"query":"{{RANDOMVARIABLE}}\n {\n  hero{\n    name\n  }\n}","variables":{},"operationName":null}'
 query_middle_complexity='{"query":"{{RANDOMVARIABLE}}\n query{\n  hero(episode: JEDI){\n    id\n    name\n    appearsIn\n    secretBackstory\n    friends{\n      id\n      name\n      ... on Droid{\n        appearsIn\n        primaryFunction\n      }\n      ... on Human{\n        appearsIn\n        secretBackstory\n      }\n      ... HumanFragment\n    }\n  }\n}\n\nfragment HumanFragment on Human{\n\tid\n    name\n    appearsIn\n    secretBackstory\n    friends{\n      id\n      name\n      ... on Droid{\n        appearsIn\n        primaryFunction\n      }\n      ... on Human{\n        appearsIn\n        secretBackstory\n      }\n    }\n}","variables":{},"operationName":null}'
 query_high_complexity='{"query":"{{RANDOMVARIABLE}}\n query{\n  human(id:\"1001\"){\n    ... HumanFragment\n  }\n  droid(id:\"2001\"){\n  \t... DroidFragment\n  }\n  hero(episode: JEDI){\n    id\n    name\n    appearsIn\n    secretBackstory\n    friends{\n      id\n      name\n      ... on Droid{\n        appearsIn\n        primaryFunction\n      }\n      ... on Human{\n        appearsIn\n        secretBackstory\n      }\n      ... HumanFragment\n    }\n  }\n}\n\nfragment HumanFragment on Human{\n\tid\n    name\n    appearsIn\n    secretBackstory\n    friends{\n      id\n      name\n      ... on Droid{\n        appearsIn\n        primaryFunction\n        friends{\n          ... CharacterFragment\n        }\n      }\n      ... on Human{\n        appearsIn\n        secretBackstory\n        friends{\n          ... CharacterFragment\n        }\n      }\n    }\n}\n\nfragment DroidFragment on Droid{\n\tid\n    name\n    appearsIn\n    primaryFunction\n    friends{\n      id\n      name\n      ... on Droid{\n        appearsIn\n        primaryFunction\n        friends{\n          ... CharacterFragment\n        }\n      }\n      ... on Human{\n        appearsIn\n        secretBackstory\n        friends{\n          ... CharacterFragment\n        }\n      }\n    }\n}\n\nfragment CharacterFragment on Character{\n  id\n  name\n  friends{\n    name\n  }\n}","variables":{},"operationName":null}'
 query_introspection='{"query":"{{RANDOMVARIABLE}}\n query IntrospectionQuery {\n            __schema {\n              queryType { name }\n              mutationType { name }\n              subscriptionType { name }\n              types {\n                ...FullType\n              }\n              directives {\n                name\n                description\n                args {\n                  ...InputValue\n                }\n              }\n            }\n          }\n        \n          fragment FullType on __Type {\n            kind\n            name\n            description\n            fields(includeDeprecated: true) {\n              name\n              description\n              args {\n                ...InputValue\n              }\n              type {\n                ...TypeRef\n              }\n              isDeprecated\n              deprecationReason\n            }\n            inputFields {\n              ...InputValue\n            }\n            interfaces {\n              ...TypeRef\n            }\n            enumValues(includeDeprecated: true) {\n              name\n              description\n              isDeprecated\n              deprecationReason\n            }\n            possibleTypes {\n              ...TypeRef\n            }\n          }\n        \n          fragment InputValue on __InputValue {\n            name\n            description\n            type { ...TypeRef }\n            defaultValue\n          }\n        \n          fragment TypeRef on __Type {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                }\n              }\n            }\n          }","variables":{},"operationName":"IntrospectionQuery"}'
 
-# SART CONFIGURATION SECTION:
-# here you can configure the query that is being sent and the number of iterations perfomed to each url
-query="$query_introspection"
-max_iteration=150
+# Here you can configure the query that is being sent and the number of iterations perfomed to each url
+# START CONFIGURATION SECTION:
+query="$query_middle_complexity"
+num_iterations=150
 # END CONFIGURATION SECTION:
 
 # START SCRIPT
@@ -22,15 +22,22 @@ escapedQuery=$(echo "$query" | sed 's/"/\\"/g')
 escapedQuery=$(echo "$escapedQuery" | sed 's/\\\\/\\/g')
 output="{\"timestamp\":\"$(date +"%Y-%m-%d %H:%M:%S")\", \"query\":\"$escapedQuery\",\"benchmarks\":["
 num_urls=2
-max_iteration=$((max_iteration+5))
+num_iterations=$((num_iterations+5))
+
+# iterate over the two servers
 for url in "http://localhost:8888/" "http://localhost:8889/"
 do
   output="$output{\"url\":\"$url\", \"data\":["
-  for i in $(seq "$max_iteration")
+  for i in $(seq "$num_iterations")
   do
+    # build temp query with random variable
     random_number=$RANDOM
     temp_query=$(echo $query | sed -e "s/${pattern}/#${random_number}/g")
-    echo -ne "url: $url | iteration: $i/$max_iteration (with 5 iter. warm-up)\r"
+
+    # print out the progress
+    echo -ne "url: $url | iteration: $i/$num_iterations (with 5 iter. warm-up)\r"
+
+    # run the actual curl
     #result=$(curl "$url" \
     result=$(curl -s -w "%{time_total}" -o /dev/null "$url" \
     -H 'Connection: keep-alive' \
@@ -45,9 +52,12 @@ do
     --data-raw "$temp_query" \
     --compressed)
     #echo "$result"
+
+    # ignore first 5 iterations for output
     if [ "$i" -gt "5" ]; then
+      # write results to output
       output="$output$result"
-      if [ "$i" -lt "$max_iteration" ]; then
+      if [ "$i" -lt "$num_iterations" ]; then
         output="$output,"
       fi
     fi
